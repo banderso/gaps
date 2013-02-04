418c83227e81eff23a797c3f7835e9f4e0ae5042
Benjamin Anderson <me@bander.so>
2013-02-04 17:01:10 -0600

    Gaps should be gap buffer oriented only.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [noise "0.1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
