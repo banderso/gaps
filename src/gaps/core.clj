d4fc94e86e161ea8cca295f8123234228161d51d
Benjamin Anderson <me@bander.so>
2013-02-04 02:11:36 -0600

    Add gap buffer protocol.

--- src/gaps/core.clj
+++ src/gaps/core.clj
@@ -1,38 +1,47 @@
 (ns gaps.core)
 
-(defrecord GapBuffer [pos before after])
+(defprotocol GapBuffer
+  "A generic gap buffer protocol."
+  (right [this] "Move the gap one element right.")
+  (left  [this] "Move the gap one element left.")
+  (insert [this c] "Insert an object to the left of the current gap.")
+  (delete [this]   "Delete an object to the left of the current gap."))
+
+(defrecord DoubleListBuffer [pos before after]
+  GapBuffer
+  (right [this]
+    (let [before (:before this)
+          after (:after this)]
+      (if (nil? (first after))
+        this
+        (DoubleListBuffer. (inc (:pos this)) 
+                           (conj before (first after))
+                           (rest after)))))
+  
+  (left  [this]
+    (let [before (:before this) 
+          after (:after this)]
+      (if (nil? (first before))
+        this
+        (DoubleListBuffer. (dec (:pos this))
+                           (rest before)
+                           (conj after (first before))))))
+  
+  (insert [this c]
+    (let [before (:before this)
+          after (:after this)]
+      (DoubleListBuffer. (inc (:pos this))
+                         (conj before c)
+                         after)))
+  
+  (delete [this]
+    (let [before (:before this)
+          after (:after this)]
+      (DoubleListBuffer. (dec (:pos this))
+                  (rest before)
+                  after))))
 
 (defn gap-buffer [string]
-  (GapBuffer. 0 '() (seq string)))
+  (DoubleListBuffer. 0 '() (seq string)))
 
-(defn right [gbuf]
-  (let [before (:before gbuf)
-        after (:after gbuf)]
-    (if (nil? (first after))
-      gbuf
-      (GapBuffer. (inc (:pos gbuf)) 
-                  (conj before (first after))
-                  (rest after)))))
 
-(defn left [gbuf]
-  (let [before (:before gbuf) 
-        after (:after gbuf)]
-    (if (nil? (first before))
-      gbuf
-      (GapBuffer. (dec (:pos gbuf))
-                  (rest before)
-                  (conj after (first before))))))
-
-(defn insert [gbuf c]
-  (let [before (:before gbuf)
-        after (:after gbuf)]
-    (GapBuffer. (inc (:pos gbuf))
-                (conj before c)
-                after)))
-
-(defn delete [gbuf]
-  (let [before (:before gbuf)
-        after (:after gbuf)]
-    (GapBuffer. (dec (:pos gbuf))
-                (rest before)
-                after)))
